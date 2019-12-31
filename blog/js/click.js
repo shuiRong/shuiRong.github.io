2095f217c2e29191406a63e573c4d3ac6c72b6cb
chenxiaobo <chenxiaobo@meizu.com>
2019-12-31 14:24:12 +0800

    Site updated: 2019-12-31 14:24:11

---
+++ blog/js/click.js
@@ -0,0 +1,78 @@
+$('document').ready(function() {
+  var a_idx = 0
+  jQuery(document).ready(function($) {
+    $('body').click(function(e) {
+      var a = new Array(
+        '富强',
+        '民主',
+        '文明',
+        '和谐',
+        '自由',
+        '平等',
+        '公正',
+        '法治',
+        '爱国',
+        '敬业',
+        '诚信',
+        '友善'
+      )
+      var $i = $('<span />').text(a[a_idx])
+      a_idx = (a_idx + 1) % a.length
+      var x = e.pageX,
+        y = e.pageY
+      $i.css({
+        'z-index': 999,
+        top: y - 20,
+        left: x,
+        position: 'absolute',
+        'font-weight': 'bold',
+        color: '#d92412',
+        'font-size': '20px'
+      })
+      $('body').append($i)
+      $i.animate(
+        {
+          top: y - 180,
+          opacity: 0
+        },
+        1500,
+        function() {
+          $i.remove()
+        }
+      )
+    })
+  })
+})
+
+var os = (function() {
+  var ua = navigator.userAgent,
+    isWindowsPhone = /(?:Windows Phone)/.test(ua),
+    isSymbian = /(?:SymbianOS)/.test(ua) || isWindowsPhone,
+    isAndroid = /(?:Android)/.test(ua),
+    isFireFox = /(?:Firefox)/.test(ua),
+    isTablet =
+      /(?:iPad|PlayBook)/.test(ua) ||
+      (isAndroid && !/(?:Mobile)/.test(ua)) ||
+      (isFireFox && /(?:Tablet)/.test(ua)),
+    isPhone = /(?:iPhone)/.test(ua) && !isTablet,
+    isPc = !isPhone && !isAndroid && !isSymbian
+  return {
+    isPc: isPc
+  }
+})()
+
+if (!os.isPc) {
+  $('#updown').hide()
+  var setTime = setInterval(function() {
+    if ($('#live2d-widget').length) {
+      $('#live2d-widget').hide()
+      clearInterval(setTime)
+    }
+  }, 200)
+}
+
+var sawaboutme = localStorage.getItem("sawaboutme");
+if(!sawaboutme && window.confirm("总结了下我的前端学习经历：大事件表(2016-7至今)，即「关于我」部分。要去看看吗？")){
+  document.location.href =  "/about.html"
+  localStorage.setItem("sawaboutme",true)
+}
---
+++ precache-manifest.b41b6669f5f5e590d78163445f31b5d3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ff7cf4a8c53e28293f90c2d6c5ad3e8",
+    "url": "/img/Flutter.9ff7cf4a.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "aa39f20b7462fa441075",
+    "url": "/js/chunk-vendors.a3feed7e.js"
+  },
+  {
+    "revision": "ad1e766df33fc2756ca6",
+    "url": "/js/app.5fe16f02.js"
+  },
+  {
+    "revision": "1744582f548f0658566004f8b2758132",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebf8eb374b9496ba6a4dc56632c92019",
+    "url": "/img/envelope.ebf8eb37.svg"
+  },
+  {
+    "revision": "1be5a9a5ce639fbafb5bfbcad7f85b84",
+    "url": "/img/Vue.1be5a9a5.svg"
+  },
+  {
+    "revision": "6f1d7b2d2c3bcab6c7ede355cc7004f1",
+    "url": "/img/Swift.6f1d7b2d.svg"
+  },
+  {
+    "revision": "669f1046f5f784cf205cdf6a67869394",
+    "url": "/img/React.669f1046.svg"
+  },
+  {
+    "revision": "d0fcd6f76cf26ab2ac41a753abc48c2d",
+    "url": "/img/JavaScript.d0fcd6f7.svg"
+  },
+  {
+    "revision": "759c9e287c49d576c67d8c4f726c4c75",
+    "url": "/img/All Projects.759c9e28.svg"
+  },
+  {
+    "revision": "ad1e766df33fc2756ca6",
+    "url": "/css/app.2eb2c2af.css"
+  }
+];
