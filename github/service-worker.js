7e52eea9bf9ea459b9dffd38c71a9c68178efb8c
shuiRong <shuiRong1997@icloud.com>
2020-01-21 22:51:28 +0800

    Site updated: 2020-01-21 22:51:27

--- github/service-worker.js
+++ github/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1a9711d1d8b4348c911cfd413259027e.js"
+  "github/precache-manifest.b34fbdbd03d76b96d62baf1d7f42814d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("github/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
