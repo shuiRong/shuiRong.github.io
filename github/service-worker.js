6dc09a815ee03687b8b5d834bccf4295d74f768b
shuiRong <shuiRong1997@icloud.com>
2020-01-22 21:35:15 +0800

    Site updated: 2020-01-22 21:35:13

--- github/service-worker.js
+++ github/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github/precache-manifest.844d2b6f40a60b6c772ca8babcb72405.js"
+  "/github/precache-manifest.13ba52cbc41476869f75b7cc045a5c71.js"
 );
 
 self.addEventListener('message', (event) => {
